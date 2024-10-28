d7eece237836647e04f59d43b96577e380e420d9
Vitalii Poliakov <95705028+vitalik211287@users.noreply.github.com>
2024-10-28 20:59:53 +0200

    done

--- phone_pook_pot/contacts.py
+++ phone_pook_pot/contacts.py
@@ -1,18 +1,11 @@
 from error_handler import input_error
+from adress_book import AddressBook, Record
 
 
-contacts = {}
-
-
-# @input_error
-# def add_contact(args):  
-#     # Додає контакт до словника.
-#     name, phone = args  
-#     contacts[name] = phone  
-#     return f"Contact {name} added."  
 
 @input_error
 def add_contact(args, book: AddressBook):
+    # Додає контакт до словника.
     name, phone, *_ = args
     record = book.find(name)
     message = "Contact updated."
@@ -25,28 +18,94 @@ def add_contact(args, book: AddressBook):
     return message
 
 
-
 @input_error
-def change_contact(args):  
-    name, new_phone = args  
-    if name in contacts:  
-        contacts[name] = new_phone  
-        return f"Contact {name} updated."  
+def change_contact(args, book: AddressBook):
+    # Змінює номер телефону контакту в book.
+    if len(args) < 2:
+        return "Please provide both name and new phone number."
+    
+    name, new_phone = args
+    record = book.find(name)
+
+    if record is not None:
+        # Заміна першого номера телефону на новий
+        if record.phones:
+            record.edit_phone(record.phones[0].value, new_phone)
+        else:
+            record.add_phone(new_phone)
+        return f"Contact {name} updated."
     else:
-        raise KeyError 
+        raise KeyError(f"Contact {name} not found.")
+
+
+
+
 
 @input_error
-def show_phone(args):  
+def show_phone(args, book: AddressBook):  
     # Показує номер телефону для вказаного контакту.
-    name = args[0]  
-    if name in contacts:  
-        return f"{name}'s phone number is {contacts[name]}."  
-    raise KeyError
+    if len(args) < 1:
+        return "Please provide a name." 
+    name = args[0]
+    record = book.find(name)
+    if record:
+        phones = ', '.join(phone.value for phone in record.phones)
+        return f"{name}'s phone numbers are: {phones}"
+    else:
+        return f"Contact {name} not found."
+    
+
+
 
 @input_error
-def show_all():  
-    # Показує всі контакти. 
-    if contacts:  
-        return "\n".join([f"{name}: {phone}" for name, phone in contacts.items()])  
-    else:  
-        return "No contacts found."  
+def show_all(book: AddressBook): 
+    # Показує всі контакти.  
+    if book.data:
+        return "Contacts:\n" + "\n".join(str(record) for record in book.data.values())
+    else:
+        return "No contacts found."
+    
+@input_error
+def birthdays(args, book):
+    upcoming_birthdays = book.get_upcoming_birthdays()
+    
+    if upcoming_birthdays:
+        return "\n".join([f"{name}: {date}" for name, date in upcoming_birthdays])
+    else:
+        return "No birthdays within the upcoming week."
+
+@input_error
+def show_birthday(args, book):
+    if len(args) < 1:
+        return "Please provide a name."
+    
+    name = args[0]
+    record = book.find(name)
+    
+    if record:
+        if record.birthday:
+            return f"{name}'s birthday is on {record.birthday}"
+        else:
+            return f"No birthday set for {name}."
+    else:
+        return f"Contact '{name}' not found."
+    
+
+
+@input_error
+def add_birthday(args, book):
+    if len(args) < 2:
+        return "Please provide both name and birthday in the format: [name] [DD.MM.YYYY]"
+    
+    name, birthday = args[0], args[1]
+    record = book.find(name)
+    
+    if record:
+        try:
+            record.set_birthday(birthday)
+            return f"Birthday for {name} added successfully."
+        except ValueError as e:
+            return str(e)  # Виведе повідомлення про неправильний формат дати
+    else:
+        return f"Contact '{name}' not found."
+   
--- phone_pook_pot/main.py
+++ phone_pook_pot/main.py
@@ -1,7 +1,9 @@
-from contacts import add_contact, change_contact, show_phone, show_all  
+from contacts import add_birthday, add_contact, birthdays, change_contact, show_birthday, show_phone, show_all
+from adress_book import AddressBook  
 
     
 def main():
+    book = AddressBook()
     print("Welcome to the assistant bot!")
     while True:
         command = input("Enter a command: ").lower()
@@ -13,15 +15,22 @@ def main():
                 print("How can I help you?")
             case "add":
                 args = input("Enter name and phone: ").split()
-                print(add_contact(args))
+                print(add_contact(args, book))
             case "change":
                 args = input("Enter name and new phone: ").split()
                 print(change_contact(args))
             case "phone":
                 args = input("Enter name: ").split()
-                print(show_phone(args))
+                print(show_phone(args, book))
             case "all":
-                print(show_all())
+                print(show_all(book))
+            case "add-birthday":
+                args = input("Enter birthday: ").split()
+                print(add_birthday(args, book))
+            case "show-birthday":
+                print(show_birthday(args, book))
+            case "birthdays":
+                print(birthdays(args, book))
             case _:
                 print("Invalid command.")
 
